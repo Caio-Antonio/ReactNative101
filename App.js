56f01581f3307a61258d7ed64f6e012849b2e82e
Caio Almeida <caioantonio0903@gmail.com>
2023-10-10 00:00:53 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esse é meu primeiro app no iphone</Text>
+      <Text>Mais um só pra ver se eu ainda lembro como usa react</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
